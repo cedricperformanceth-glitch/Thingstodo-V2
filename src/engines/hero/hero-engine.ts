189cb171fe56cd7ef6ad98417ca32905b0520644
cedricperformanceth-glitch <cedric.performance.th@gmail.com>
2026-08-11 12:25:14 +0700

    Remove obsolete Hero presentation dependency

--- src/engines/hero/hero-engine.ts
+++ src/engines/hero/hero-engine.ts
@@ -1,5 +1,4 @@
 import type { City } from '../../core/models/types';
-import { cityPresentation } from '../spa/city-presentation';
 import { getCityHeroAssets } from './hero-assets';
 import { getCityHeroCopy } from './hero-copy';
 import { getCityHeroFacts } from './hero-facts';
@@ -8,7 +7,6 @@ import { getCityHeroPartners } from './hero-partners';
 export const getHero = (city: City) => ({
   ...city.hero,
   profile: city.profile,
-  presentation: cityPresentation[city.profile],
   cityAssets: getCityHeroAssets(city),
   copy: getCityHeroCopy(city),
   displayFacts: getCityHeroFacts(city),
