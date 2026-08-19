85c9053b6ea552076b8c17a6986ffa686ae619af
cedricperformanceth-glitch <cedric.performance.th@gmail.com>
2026-08-19 22:32:23 +0700

    Use typed Tad Lo generated exports

--- src/content/generated/index.ts
+++ src/content/generated/index.ts
@@ -1,6 +1,6 @@
 import type { City, Place, ThingToDo } from '../../core/models/types';
 import { city as city0, places as places0, things as things0 } from './laos/don-det';
-import { city as city1, places as places1, things as things1 } from './laos/tad-lo';
+import { typedCity as city1, typedPlaces as places1, typedThings as things1 } from './laos/tad-lo';
 import { city as city2, places as places2, things as things2 } from './laos/thakhek';
 export const generatedCities: City[] = [city0, city1, city2];
 export const generatedPlaces: Place[] = [...places0, ...places1, ...places2];
