6d22a699d94119b1898ea3093afddb51033a52b3
cedricperformanceth-glitch <cedric.performance.th@gmail.com>
2026-08-22 11:30:29 +0700

    Filter removed activities from the Thing registry

--- src/content/registry/things-to-do.ts
+++ src/content/registry/things-to-do.ts
@@ -1,3 +1,7 @@
 import { generatedThings } from '../generated';
+import thingStatusOverrides from '../thing-status-overrides.json';
 import type { ThingToDo } from '../../core/models/types';
-export const things: ThingToDo[] = generatedThings;
+
+const isActiveThing = (thing: ThingToDo) => thingStatusOverrides[thing.id as keyof typeof thingStatusOverrides] !== 'removed';
+
+export const things: ThingToDo[] = generatedThings.filter(isActiveThing);
