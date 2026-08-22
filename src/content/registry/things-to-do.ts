09196a3baded52a702c7c98b345b7d76d28c50f4
cedricperformanceth-glitch <cedric.performance.th@gmail.com>
2026-08-22 18:33:36 +0700

    Remove retired Thing status filter from registry

--- src/content/registry/things-to-do.ts
+++ src/content/registry/things-to-do.ts
@@ -1,7 +1,4 @@
 import { generatedThings } from '../generated';
-import thingStatusOverrides from '../thing-status-overrides.json';
 import type { ThingToDo } from '../../core/models/types';
 
-const isActiveThing = (thing: ThingToDo) => thingStatusOverrides[thing.id as keyof typeof thingStatusOverrides] !== 'removed';
-
-export const things: ThingToDo[] = generatedThings.filter(isActiveThing);
+export const things: ThingToDo[] = generatedThings;
