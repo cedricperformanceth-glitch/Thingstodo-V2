27f1346bdb819b89dacf24504d33ec27cc66845e
cedricperformanceth-glitch <cedric.performance.th@gmail.com>
2026-08-24 19:25:31 +0700

    Register Thailand Cambodia and Vietnam

--- src/content/registry/countries.ts
+++ src/content/registry/countries.ts
@@ -1,3 +1,6 @@
 import { laos } from '../countries/laos';
+import { thailand } from '../countries/thailand';
+import { cambodia } from '../countries/cambodia';
+import { vietnam } from '../countries/vietnam';
 import type { Country } from '../../core/models/types';
-export const countries: Country[] = [laos];
+export const countries: Country[] = [laos, thailand, cambodia, vietnam];
