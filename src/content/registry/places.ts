fbc3b1688e49418f57f4e1e11b522d80dbb6cfe5
cedricperformanceth-glitch <cedric.performance.th@gmail.com>
2026-08-26 12:16:06 +0700

    Apply Thakhek place card editorial overrides

--- src/content/registry/places.ts
+++ src/content/registry/places.ts
@@ -1,3 +1,15 @@
 import { generatedPlaces } from '../generated';
+import { placeCardEditorial, supplementalPlaces } from '../place-card-editorial';
 import type { Place } from '../../core/models/types';
-export const places: Place[] = generatedPlaces;
+
+const generatedIds = new Set(generatedPlaces.map((place) => place.id));
+
+const applyEditorial = (place: Place): Place => {
+  const override = placeCardEditorial[place.id];
+  return override ? { ...place, ...override } : place;
+};
+
+export const places: Place[] = [
+  ...generatedPlaces.map(applyEditorial),
+  ...supplementalPlaces.filter((place) => !generatedIds.has(place.id)).map(applyEditorial),
+];
